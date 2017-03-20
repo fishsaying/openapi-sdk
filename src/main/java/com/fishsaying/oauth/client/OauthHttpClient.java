f2edef6987865b4012f142b21fad36d5998c6482
wangkai <wangkai@fishsaying.com>
2017-03-20 11:40:50 +0800

    完善代码

--- src/main/java/com/fishsaying/oauth/client/OauthHttpClient.java
+++ src/main/java/com/fishsaying/oauth/client/OauthHttpClient.java
@@ -1,8 +1,14 @@
 package com.fishsaying.oauth.client;
 
+import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
+import com.sun.org.apache.regexp.internal.recompile;
+
+import okhttp3.Interceptor;
 import okhttp3.OkHttpClient;
+import okhttp3.Response;
+import okhttp3.Interceptor.Chain;
 
 
 /**
@@ -18,9 +24,17 @@ public class OauthHttpClient {
 	 * @return
 	 */
 	public static OkHttpClient getClient(){
-		OkHttpClient client = new OkHttpClient();
+		OkHttpClient client = new OkHttpClient().newBuilder()
 		// 借助构建器builder 设置更多的client参数
-		client.newBuilder().connectTimeout(30, TimeUnit.SECONDS).build();
+//		client.newBuilder().addInterceptor(new Interceptor() {
+//			
+//			public Response intercept(Chain chain) throws IOException {
+//				// TODO Auto-generated method stub
+//				chain.request().
+//				return null;
+//			}
+//		})
+		.connectTimeout(30, TimeUnit.SECONDS).build();
 		return client;
 	}
 
